6cb82c96ee6116cc2c1d4c6ce391b28e0c26352f
Naman Sethi <namansethi@cmail.carleton.ca>
2018-07-28 10:59:48 -0400

    Its ya boi, master version commit numero uno

---
+++ Project/src/Controller.java
@@ -0,0 +1,26 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
+public class Controller {
+
+    @FXML
+    private Label title;
+    @FXML
+    private RadioButton choice1;
+    @FXML
+    private RadioButton choice2;
+    @FXML
+    private RadioButton choice3;
+    @FXML
+    private RadioButton choice4;
+
+
+
+    @FXML
+    public void initialize() {
+
+
+
+    }
+}
