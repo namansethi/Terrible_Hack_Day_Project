eafc2b3256e44ca8de03f49942364a39e6208056
Pruthvi <pruthvichivukula@cmail.carleton.ca>
2018-07-28 11:24:45 -0400

    Naman the destroyer

---
+++ Project/src/QuizClass.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class QuizClass {
+    Question questOne = new Question();
+    Question questTwo = new Question();
+    Question questThree = new Question();
+    Question questFour = new Question();
+    Question questFive = new Question();
+    Question questSix = new Question();
+    Question questSeven = new Question();
+    Question questEight = new Question();
+    Question questNine = new Question();
+    Question questTen = new Question();
+    Question []allQuestions = {questOne, questTwo, questThree, questFour, questFive, questSix, questSeven, questEight, questNine, questTen};
+}
